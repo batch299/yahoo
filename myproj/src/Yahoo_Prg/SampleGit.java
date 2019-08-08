fdf96984aeee88c98e230b601a0026c2d6be0994
batch299 <krishnamohanvenkat9@gmail.com>
2019-08-08 08:35:41 +0530

    updated sample

--- myproj/src/Yahoo_Prg/SampleGit.java
+++ myproj/src/Yahoo_Prg/SampleGit.java
@@ -1,7 +1,11 @@
 package Yahoo_Prg;
 
-public class SampleGit {
-
+public class SampleGit
+{
+    public void f1()
+    {
+    	System.out.println("this is f1");
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
