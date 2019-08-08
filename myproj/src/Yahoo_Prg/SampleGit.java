c6295d396425992bdb3ecb086539d9f7e4c7f2b1
S@F <S@F@192.168.100.125>
2019-08-08 08:38:35 +0530

    BCD

--- myproj/src/Yahoo_Prg/SampleGit.java
+++ myproj/src/Yahoo_Prg/SampleGit.java
@@ -1,6 +1,11 @@
 package Yahoo_Prg;
 
 public class SampleGit {
+	public void F2() {
+		
+		System.out.println("Update F2");
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
