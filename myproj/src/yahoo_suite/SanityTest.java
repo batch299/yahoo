069c68c7b2912873552c1828ecb41ed83961b453
batch299 <krishnamohanvenkat9@gmail.com>
2019-08-08 08:05:00 +0530

    1st time updating the project

---
+++ myproj/src/yahoo_suite/SanityTest.java
@@ -0,0 +1,52 @@
+package yahoo_suite;
+
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import Yahoo_Prg.Compose;
+import Yahoo_Prg.Login;
+import Yahoo_Prg.MainClass;
+
+public class SanityTest extends MainClass
+{
+  @Test	
+  @Parameters({"browser"})
+  public void sanitytesting(String str)  throws Exception
+  {
+	  if(str.matches("firefox"))
+	  {
+		  driver=new FirefoxDriver();
+	  }
+	  if(str.matches("chrome"))
+	  {
+		  driver=new ChromeDriver();
+	  }
+	  FileInputStream fin = new FileInputStream("D:\\sel_dec18\\Testcases.xlsx");
+	  XSSFWorkbook wb=new XSSFWorkbook(fin);
+	  XSSFSheet ws=wb.getSheet("sanitytest");
+	  Row row;
+	  String classname,methodname;
+	  for(int r=1;r<=ws.getLastRowNum();r++)
+	  {
+		  row=ws.getRow(r);
+		  if(row.getCell(5).getStringCellValue().matches("yes"))
+		  {
+			  classname=row.getCell(3).getStringCellValue();
+			  methodname=row.getCell(4).getStringCellValue();
+			  Class c=Class.forName(classname);  //load Sample class into memory
+			  Method m=c.getMethod(methodname,null); // get method in the class
+			  Object obj=c.newInstance(); //create instance for the class
+			  m.invoke(obj, null);  			  
+		  }
+	  }
+	  fin.close();
+  }
+}
